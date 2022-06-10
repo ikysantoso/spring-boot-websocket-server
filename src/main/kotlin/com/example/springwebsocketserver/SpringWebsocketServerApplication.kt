b09b4450d2857ada3612bf02e78fc61dbbce083c
Dicky Santoso <ikysantoso1@gmail.com>
2022-06-10 23:28:19 +0700

    project: init

---
+++ src/main/kotlin/com/example/springwebsocketserver/SpringWebsocketServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springwebsocketserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebsocketServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringWebsocketServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springwebsocketserver/SpringWebsocketServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springwebsocketserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringWebsocketServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
